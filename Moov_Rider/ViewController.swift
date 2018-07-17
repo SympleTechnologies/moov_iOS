59b9cfba8e0f1682c74b0052a395e97eac32a609
Visakh <krishnadaspisharam@visakhs-mac-mini.local>
2018-07-17 12:05:10 +0530

    Initial Commit

---
+++ Moov_Rider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moov_Rider
+//
+//  Created by Visakh on 17/07/18.
+//  Copyright © 2018 Visakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
